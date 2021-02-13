04bdfb4ccc15791c35651e0e5ac57f889ee7ef91
jitendra <jitendrapandey1730@gmail.com>
2021-02-13 02:36:00 -0500

    Adding services.

--- pet-clinic-data/src/main/java/petclinc/services/OwnerService.java
+++ pet-clinic-data/src/main/java/petclinc/services/OwnerService.java
@@ -1,5 +1,13 @@
 package petclinc.services;
 
+import petclinic.model.Owner;
+
+import java.util.Set;
+
 public interface OwnerService {
-    find
+    Owner findById(Long id);
+    Owner save(Owner owner);
+    Set<Owner> findAll();
+    Owner findByLastName(String lastName);
+
 }
